7b0c53035ebefd7a06d4507bada6705ada665e3c
AdmSyd <adm_syd@icloud.com>
2025-06-14 06:32:50 +0500

    init commit

--- src/main/java/org/fp/Main.java
+++ src/main/java/org/fp/Main.java
@@ -1,12 +1,10 @@
 package org.fp;
 
-import org.fp.container.ApplicationContext;
 import org.fp.service.statistics.Statistics;
 
 public class Main {
     public static void main(String[] args) {
         Facade.start();
-        Statistics statistics = (Statistics) ApplicationContext.getDependency("Statistics1");
-        System.out.println(statistics.getStatistic());
+        System.out.println(Statistics.getStatistic());
     }
 }
